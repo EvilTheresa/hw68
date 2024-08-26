6758aa027e6fdeb7382551a43a024f7fed1c32a3
helux <kled.master@gmail.com>
2024-08-26 18:58:39 +0600

    Урлы

--- source/api_v2/urls.py
+++ source/api_v2/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from api_v2.views.articles import get_csrf_token
+from api_v2.views.comments import CommentListView, CommentCreateView, CommentDetailView, CommentUpdateView, \
+    CommentDeleteView
 from source.api_v2.views import ArticleUpdateView, ArticleCreateView, ArticleDeleteView, \
     ArticleDetailView
 
@@ -12,4 +14,10 @@ urlpatterns = [
     path('articles/<int:pk>/update/', ArticleUpdateView.as_view(), name='article'),
     path('articles/<int:pk>/add/', ArticleCreateView.as_view(), name='article'),
     path('articles/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article'),
+
+    path('articles/<int:article_id>/comments/', CommentListView.as_view(), name='article'),
+    path('articles/<int:article_id>/comments/add/', CommentCreateView.as_view(), name='article'),
+    path('comments/<int:pk>/', CommentDetailView.as_view(), name='article'),
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='article'),
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='article')
 ]
