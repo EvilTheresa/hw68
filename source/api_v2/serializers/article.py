c4a2477cdeb587d407e360fdee1179d1f4ffb9bd
helux <kled.master@gmail.com>
2024-08-26 16:59:09 +0600

    Создал приложение и сериалайзер для статьи

---
+++ source/api_v2/serializers/article.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from article_projects.source.webapp.models.article import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'text', 'created_at', 'updated_at', 'author']
